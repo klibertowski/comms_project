1d5700541ebaecb7cd0d3239aef3747263d6b79e
Kevin Libertowski <klibertowski@customorthopaedics.com>
2020-04-13 16:42:31 -0400

    cleanup

--- comms_project.m
+++ comms_project.m
@@ -2,14 +2,10 @@ g1 = @(t) (1000*t - 2).^2;
 g2 = @(t) (-5000*t + 22).^2;
 g3 = @(t) (3000*t - 18).^2;
 
-E_total = integral(g1, .002, .004) + integral(g2, .004, .005) + integral(g3, .005, .006)
+E_eq = integral(g1, .002, .004) + integral(g2, .004, .005) + integral(g3, .005, .006);
 
-t = 0:.00001:.006;
-% gt = @(t) abs((2*tripuls(t - .004, .004) - 4*tripuls(t - .005, .002))).^2;
-gt = 2*tripuls(t - .004, .004) - 4*tripuls(t - .005, .002);
-
-plot(t, gt)
-% E_total = integral(gt, 0, .006)
+gt_sq = @(t) abs((2*tripuls(t - .004, .004) - 4*tripuls(t - .005, .002))).^2;
+E_tripuls = integral(gt_sq, 0, .006);
 
 Gf_sq = @(f) abs((-.004*sinc((pi*.002*f)/2).^2 .* exp(-1i*pi*5*f)).^2);
 
@@ -23,11 +19,3 @@ while E_ess < .99*E_total
     B_ess = B_ess + B_step;
     E_ess = integral(Gf_sq, -B_ess, B_ess)
 end
-
-% test = abs(integral(Gf, 1, 3))
-
-% f = -2:.001:2;
-% Gf = (.004*sinc((pi*.004*f)/2).^2 .* exp(-1i*2*pi*f) + -.004*sinc((pi*.002*f)/2).^2 .* exp(-1i*pi*5*f)).^2;
-% Gf = (.004*sinc((pi*.004*f)/2).^2 .* exp(-1i*2*pi*f) + -.004*sinc((pi*.002*f)/2).^2 .* exp(-1i*pi*5*f)).^2;
-% 
-% plot(f, Gf)
