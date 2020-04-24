9a51c38f984bd41f1dd4908a733c16c647463440
Kevin Libertowski <klibertowski@customorthopaedics.com>
2020-04-24 17:16:55 -0400

    essential bw plot

--- comms_project.m
+++ comms_project.m
@@ -19,22 +19,9 @@ E_gt = integral(g1, .002, .004) + integral(g2, .004, .005) + integral(g3, .005, 
 gt_sq = @(t) abs((2*tripuls(t - .004, .004) - 4*tripuls(t - .005, .002))).^2;
 
 % Plot of g(t)
-t = 0:.000001:.006;
-gt = 2*tripuls(t - .004, .004) - 4*tripuls(t - .005, .002);
+% t = 0:.0001:.04;
+% gt = 2*tripuls(t - .004, .004) - 4*tripuls(t - .005, .002);
 % plot(t, gt)
-
-n = 100000;
-Gf_fft = fft(gt, n);
-plot(linspace(0,.00001, n/500), abs(Gf_fft(1:n/500)))
-
-subplot(2,1,1)
-
-t = 0:.000001:.006;
-Gf = .004*sinc((.004*t)/2).^2 .* exp(-1i*pi*2*t*.004) - .004*sinc((.002*t)/2).^2 .* exp(-1i*pi*.005*t);
-plot(linspace(0,.00001, n/500), abs(Gf(1:n/500)))
-subplot(2,1,2)
-
-legend('show')
 
 % Energy from triangular pulse waveform
 E_tripuls = integral(gt_sq, 0, .006);
@@ -57,4 +44,21 @@ format long
 % calculated from while loop
 B_ess_cal = 694.2;
 
+% oversample essential bandwidth
+BW = 1400;
 
+t = 0:1/BW:.04;
+gt = 2*tripuls(t - .004, .004) - 4*tripuls(t - .005, .002);
+stem(t, gt)
+
+% n = 100000;
+% Gf_fft = fft(gt, n);
+% plot(t, g)
+% 
+% subplot(2,1,1)
+% 
+% Gf = .004*sinc((.004*t)/2).^2 .* exp(-1i*pi*2*t*.004) - .004*sinc((.002*t)/2).^2 .* exp(-1i*pi*2*.005*t);
+% plot(linspace(0,.00001, n/500), abs(Gf(1:n/500)))
+% subplot(2,1,2)
+% 
+% legend('show')
